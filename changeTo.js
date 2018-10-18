fc2964df0033c71c3a29b06726ece4339539bf5c
Jacob Greenwald <jeg4t@virginia.edu>
2018-10-18 14:41:37 -0400

    messing with storage

--- changeTo.js
+++ changeTo.js
@@ -1,5 +1,5 @@
 console.log('change to', $)
-let value = 'test'
+let value = [{'test': 'test'}, 'testing', 'anther test']
 chrome.storage.sync.set({key: value}, function() {
     console.log('Value is set to ' + value);
   });
