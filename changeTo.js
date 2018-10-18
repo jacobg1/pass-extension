7a66ef47c584ee07839950769763b0feed94e7dc
Jacob Greenwald <jeg4t@virginia.edu>
2018-10-18 17:02:41 -0400

    add password selector

--- changeTo.js
+++ changeTo.js
@@ -1,5 +1,7 @@
 console.log('change to', $)
-let value = [{'test': 'test'}, 'testing', 'anther test']
+let value = ['test'];
+let allPasswordInputs = $('input:password')
+console.log(allPasswordInputs)
 chrome.storage.sync.set({key: value}, function() {
     console.log('Value is set to ' + value);
   });
