2b10b9fad99a06e845a5eaa187d1ddf2d2bb6fd8
Jacob Greenwald <jeg4t@virginia.edu>
2018-10-19 00:29:54 -0400

    testing

--- changeFrom.js
+++ changeFrom.js
@@ -2,13 +2,16 @@ console.log('change from', $)
 
 
 
-chrome.storage.sync.get(['key'], function(result) {
-    console.log('Value currently is ' + result.key);
-    if(result.key) {
-        chrome.storage.sync.clear(function() {
-            console.log('storage cleared', result.key);
-          });
+// chrome.storage.sync.get("object", function(result) {
+//     console.log(result);
+//     // if(result.key) {
+//     //     chrome.storage.sync.clear(function() {
+//     //         console.log('storage cleared', result.key);
+//     //       });
 
-    }
-  })
+//     // }
+//     // $.each(result.key, function (index, value) {
+//     //    console.log(value);
+//     // });
+//   })
 
--- changeTo.js
+++ changeTo.js
@@ -1,8 +1,27 @@
 console.log('change to', $)
-let value = ['test'];
-let allPasswordInputs = $('input:password')
-console.log(allPasswordInputs)
-chrome.storage.sync.set({key: value}, function() {
-    console.log('Value is set to ' + value);
-  });
+// if (typeof (allPasswordInputs) === 'undefined') {
+//   var allPasswordInputs = $("input[type='password']")
+//   console.log(allPasswordInputs)
+//   chrome.storage.sync.set({
+//     "object": allPasswordInputs
+//   }, function () {
+//     console.log(allPasswordInputs);
+//   });
+// }
+  
 
+
+// console.log(allPasswordInputs)
+
+
+// chrome.storage.sync.clear(function () {
+//   console.log('storage cleared', result.key);
+// });
+if (typeof (allPasswordInputs) === 'undefined') {
+    var allPasswordInputs = $("input[type='password']")
+}
+
+allPasswordInputs.each(function(index, value) {
+  console.log(value)
+  value.attr('type', 'text')
+})
