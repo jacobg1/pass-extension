79aa252f11c54756ef499e728b4ac7a4d3456ef4
Jacob Greenwald <jeg4t@virginia.edu>
2018-11-02 17:16:49 -0400

    add link selectors and buttons

--- popup.js
+++ popup.js
@@ -43,3 +43,42 @@ let changeTo = document.getElementById('changeTo');
         });
     });
   };
+  let changeLinkTo = document.getElementById('changeLinkTo');
+// chrome.storage.sync.get('color', function(data) {
+//   changeColor.style.backgroundColor = data.color;
+//   changeColor.setAttribute('value', data.color);
+// });
+  changeLinkTo.onclick = function (element) {
+    let color = element.target.value;
+    chrome.tabs.query({
+      active: true,
+      currentWindow: true
+    }, function (tabs) {
+      chrome.tabs.executeScript(
+        tabs[0].id, {
+          file: 'jquery/jquery-3.3.1.min.js'
+        }, function () {
+           chrome.tabs.executeScript({
+             file: 'changeLinkTo.js'
+           });
+        });
+    });
+  };
+
+  let changeLinkFrom = document.getElementById('changeLinkFrom');
+
+  changeLinkFrom.onclick = function (element) {
+    chrome.tabs.query({
+      active: true,
+      currentWindow: true
+    }, function (tabs) {
+      chrome.tabs.executeScript(
+        tabs[0].id, {
+          file: 'jquery/jquery-3.3.1.min.js'
+        }, function () {
+           chrome.tabs.executeScript({
+             file: 'changeLinkFrom.js'
+           });
+        });
+    });
+  };
